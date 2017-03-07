fe18313b9a7b6111b54a836360ddcdfc2d8dec33
Austin Blain <austin.a.blain@gmail.com>
2017-03-07 14:58:38 -0800

    add styling and sidebar to class page

--- src/app/class-page/class-page.component.ts
+++ src/app/class-page/class-page.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 import { Class } from './../class.model';
 import { ClassService } from '../class.service';
 import { FirebaseObjectObservable } from 'angularfire2';
+declare var $:any;
 
 
 @Component({
@@ -30,6 +31,27 @@ export class ClassPageComponent implements OnInit {
       this.classId = urlParametersArray['id'];
     });
     this.classToDisplay = this.classService.getClassById(this.classId);
+
+    for (var i=1; i<10; i++) {
+      $(".classPageBox__seatingChartBox__seatingChart").append('<div class="classPageBox__seatingChartBox__seatingChart__row" id="' + i + '"></div>');
+      for (var j=1; j<10; j++) {
+        $(".classPageBox__seatingChartBox__seatingChart__row:last-of-type").append('<div class="classPageBox__seatingChartBox__seatingChart__row__seatSpot' + ' r' + i + 'c' + j + '"></div>');
+      }
+    };
+  }
+
+  showThatChartList() {
+    $(".notShown").addClass("showing");
+    $(".notShown").removeClass("notShown");
+  }
+
+  hideTheSidebarPlease() {
+    if ($(".classPageBox__sidebar").hasClass("showing") === true) {
+      $(".showing").addClass("notShown");
+      $(".showing").removeClass("showing");
+    } else {
+      
+    }
   }
 
 }
