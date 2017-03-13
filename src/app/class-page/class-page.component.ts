f144371ee84054aceaf97593901576127630b767
Austin Blain <austin.a.blain@gmail.com>
2017-03-13 12:34:54 -0700

    remove commented out code in all components

--- src/app/class-page/class-page.component.ts
+++ src/app/class-page/class-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Directive, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Class } from './../class.model';
 import { ClassService } from '../class.service';
-import { FirebaseObjectObservable } from 'angularfire2';
+import { FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 declare var $:any;
 
 
@@ -13,26 +14,28 @@ declare var $:any;
   styleUrls: ['./class-page.component.css'],
   providers: [ClassService]
 })
+
+
 export class ClassPageComponent implements OnInit {
 
   classId: string;
   classToDisplay: FirebaseObjectObservable<Class>;
   classToCall: FirebaseObjectObservable<Class>;
+  classes: FirebaseListObservable<any[]>;
+
   @Input() selectedClass;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private classService: ClassService
-  ) { }
-
+  ) {  }
 
   ngOnInit() {
     this.route.params.forEach((urlParametersArray) => {
       this.classId = urlParametersArray['id'];
     });
     this.classToDisplay = this.classService.getClassById(this.classId);
-
     for (var i=1; i<10; i++) {
       $(".classPageBox__seatingChartBox__seatingChart").append('<div class="classPageBox__seatingChartBox__seatingChart__row" id="' + i + '"></div>');
       for (var j=1; j<10; j++) {
@@ -48,7 +51,6 @@ export class ClassPageComponent implements OnInit {
   }
 
   hideTheSidebarPlease() {
-    console.log(this);
     if ($(".classPageBox__sidebar").hasClass("showing") === true) {
       $(".showing").addClass("notShown");
       $(".showing").removeClass("showing");
@@ -67,12 +69,25 @@ export class ClassPageComponent implements OnInit {
   }
 
   showThisChart(chartClicked) {
-    console.log(chartClicked.students.length);
-    console.log();
-    // for (var x; x<chartClicked.students.length; x++) {
-    //   var currentPlace;
-    //   var currentOrder;
-    // }
+    console.log(chartClicked.students);
+    $(".classPageBox__seatingChartBox__seatingChart__row__seatSpot").empty();
+    for (var i = 0; i<chartClicked.students.length; i++) {
+      console.log(chartClicked.students[i].place);
+      $('.' + chartClicked.students[i].place).append('<span draggable="true">' + chartClicked.students[i].order + '</span>');
+    }
+  }
+
+  dragStart() {
+
+
+  }
+
+  spinGenerateButton() {
+    if ($(".classPageBox__topBoxes__classActions__buttonsBox__generateButton__generateIcon").hasClass("spinIt") === false) {
+      $(".classPageBox__topBoxes__classActions__buttonsBox__generateButton__generateIcon").addClass("spinIt");
+    } else if ($(".classPageBox__topBoxes__classActions__buttonsBox__generateButton__generateIcon").hasClass("spinIt") === true) {
+      $(".classPageBox__topBoxes__classActions__buttonsBox__generateButton__generateIcon").removeClass("spinIt");
+    }
   }
 
 }
